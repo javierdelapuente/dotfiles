46e560a70f44d2965750413f9a65a6460754e21f
Javier de la Puente <javier.delapuente@canonical.com>
2024-10-19 10:45:41 +0200

    changes in emacs init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -69,6 +69,7 @@
 (use-package protobuf-mode)
 (use-package markdown-mode)
 (add-hook 'markdown-mode-hook (lambda () (setq-local require-final-newline nil)))
+(advice-add 'markdown-preview :around (lambda (orig &rest args) "Use chromium as default browser" (let ((browse-url-browser-function #'browse-url-chrome)) (apply orig args))))
 
 (use-package ace-window
   :defer t
@@ -123,7 +124,7 @@
 ;; - sudo apt install python3-pylsp 
 
 ;; For go:
-;; - go get golang.org/x/tools/gopls@latest
+;; - go install golang.org/x/tools/gopls@latest
 
 ;; For js:
 ;; install ts-ls when queried after opening a js/ts buffer with lsp
@@ -194,12 +195,28 @@
 (use-package term
   :bind (:map term-raw-map ("C-c C-y" . term-paste)))
 
+(use-package python-pytest)
+;; https://gist.githubusercontent.com/grafov/10985431/raw/dcd12037308446179f26f7d2ab2c034a1e995d2e/go-template-mode.el
+(use-package go-template-mode   :ensure nil  :load-path "~/.config/emacs/go-template-mode.el")
+
+
 (use-package mermaid-mode
   :config
   (setq mermaid-mmdc-location "/usr/bin/npx")
     ;; FIXME Will not work with org mode, as mermaid-flags are the last arguments to the program
   (setq mermaid-flags (format "-p @mermaid-js/mermaid-cli mmdc -p %s/.config/emacs/puppeteer-config.json" (getenv "HOME")))
   )
+
+
+;; https://emacs-lsp.github.io/dap-mode/page/configuration/#dap-mode-configuration
+;; go install github.com/go-delve/delve/cmd/dlv@latest
+
+(use-package dap-mode
+  :config
+  (require 'dap-dlv-go)
+  (dap-auto-configure-features t)
+  )
+
 
 ;; ==================================================
 ;; Some hardcoded configuration. Look for alternatives
@@ -268,18 +285,27 @@
 (advice-add 'tramp-get-completion-function :around #'remove-multipass-delete-default-host)
 
 
+(setq auto-mode-alist (cons '("\\.csproj$" . nxml-mode) auto-mode-alist))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-src-preserve-indentation t)
  '(package-selected-packages
-   '(mermaid-mode python-black ox-hugo tox apache-mode jinja2-mode typescript-mode terraform-mode lsp-mode corfu pyvenv google-this which-key flycheck helm git-link forge magit ace-window markdown-mode protobuf-mode go-mode dockerfile-mode hcl-mode yaml-mode quelpa-use-package material-theme quelpa diminish))
+   '(dap-mod dap-mode dap-dlv-go python-pytest mermaid-mode python-black ox-hugo tox apache-mode jinja2-mode typescript-mode terraform-mode lsp-mode corfu pyvenv google-this which-key flycheck helm git-link forge magit ace-window markdown-mode protobuf-mode go-mode dockerfile-mode hcl-mode yaml-mode quelpa-use-package material-theme quelpa diminish))
  '(safe-local-variable-values
-   '((etags-regen-ignores "test/manual/etags/")
+   '((eval set
+	   (make-local-variable 'flycheck-go-build-tags)
+	   '("libsqlite3"))
+     (go-test-args . "-tags libsqlite3 -timeout 120s")
+     (etags-regen-ignores "test/manual/etags/")
      (etags-regen-regexp-alist
       (("c" "objc")
-       "/[ \11]*DEFVAR_[A-Z_ \11(]+\"\\([^\"]+\\)\"/\\1/" "/[ \11]*DEFVAR_[A-Z_ \11(]+\"[^\"]+\",[ \11]\\([A-Za-z0-9_]+\\)/\\1/")))))
+       "/[ \11]*DEFVAR_[A-Z_ \11(]+\"\\([^\"]+\\)\"/\\1/" "/[ \11]*DEFVAR_[A-Z_ \11(]+\"[^\"]+\",[ \11]\\([A-Za-z0-9_]+\\)/\\1/"))))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
