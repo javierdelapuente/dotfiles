bf5e19081cc49762b6856583ce72f2d712759103
Javier de la Puente <javier@delapuente.es>
2025-12-15 09:57:59 +0100

    add bashrc similar to the main branch

--- .bashrc.dotfiles.bash
+++ .bashrc.dotfiles.bash
@@ -1,5 +1,5 @@
 # Add:
-# . .bashrc.dotfiles.bash
+# . "$HOME/.bashrc.dotfiles.bash"
 # to your .bashrc file.
 
 HISTSIZE=100000
@@ -21,3 +21,15 @@ fi
 
 source /usr/share/bash-completion/completions/git
 __git_complete dotfiles __git_main
+
+# pnpm
+export PNPM_HOME="/home/jpuente/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
